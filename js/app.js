748ea6494c49b5b6b0abea8e154ea717beb6d54a
Nuradin Ibrahim <nuradinibrahim@Nuradins-MacBook-Air.local>
2020-08-25 07:52:53 -0600

    added functions

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function nums(){
+    console.log((2 + 2) *5)
+}
+nums()
+
+
+function TrueorFalse(){
+    console.log('Hello my name is Nuradin')
+}if(TrueorFalse.length <10){
+false
+}
+
+TrueorFalse()
+
+function strings (){
+    console.log(
+        (
+            "Hello how are you today?",
+            "Im great how are you?",
+            "phone me later"
+        )
+    )
+}
+strings()
