4126d1ff1c744a53beaf14ded1644e0d63846699
Nuradin Ibrahim <nuradinibrahim@Nuradins-MacBook-Air.local>
2020-08-26 23:23:15 -0600

    added quary selectors

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
-function nums(){
-    console.log((2 + 2) *5)
-}
-nums()
+var firstHtag = document.querySelector('h1');
+firstHtag.innerHTML = "Hello world";
 
-
-function TrueorFalse(){
-    console.log('Hello my name is Nuradin')
-}if(TrueorFalse.length <10){
-false
+var allPTags = document.querySelectorAll('p');
+for(var i=0; i<allPTags.length; i++){
+    allPTags[i].style.color='red';
 }
 
-TrueorFalse()
+var header =document.getElementById('header');
+header.style.fontSize= '3.5em';
 
-function strings (){
-    console.log(
-        (
-            "Hello how are you today?",
-            "Im great how are you?",
-            "phone me later"
-        )
-    )
-}
-strings()
+var image =document.getElementsByClassName('image')[0]
+image.style.width = '300px'
+image.style.height = '300px'
+
+var h3 =document.getElementsByTagName('h3') [0];
+h3.innerHTML = "Hey there my name is Nuradin"
+
+var newdiv = document.createElement('div')
+var injectedContent = document.createTextNode('Hell there')
+newdiv.appendChild(injectedContent)
+var parentdiv = document.getElementById('parent');
+document.body.insertBefore(newdiv, parentdiv);
